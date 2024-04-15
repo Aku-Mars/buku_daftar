a6fb5d254cecdac7db590396bab99f1f3400b621
Mars <76494850+Aku-Mars@users.noreply.github.com>
2024-04-15 17:54:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Pemweb</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body> 
+    <div class="wrapper-menu">
+        <div class="menu-container">
+            <h2 class="title"><?php echo "Selamat datang di Session login Tamu kami"?></h2>
+            <ul class="menu">
+                <li>
+                    <a href="add_guest.php">Tambah Tamu</a>
+                </li>
+                <li>
+                    <a href="view_guest.php">Lihat Tamu</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
